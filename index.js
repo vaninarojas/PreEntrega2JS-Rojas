76151ed926e56eb86d5d8547e4257c9236271d63
vaninarojas <vanina_93@hotmail.com>
2023-07-09 17:27:36 -0300

    Pre Entrega 2 Js

---
+++ index.js
@@ -0,0 +1,49 @@
+addProductosPrompt = () => {
+  let productos = [];
+
+  while (true) {
+    let nombre = prompt("Ingrese nombre del producto (o 'salir' para finalizar): ");
+    
+    if (nombre === "salir") {
+      break;
+    }
+
+    let categoria = prompt("Ingrese categoría del producto: ");
+    let precio = parseFloat(prompt("Ingrese precio del producto: "));
+    let stock = Number(prompt("Ingrese stock del producto: "));
+
+    let producto = { nombre, categoria, precio, stock };
+    productos.push(producto);
+  }
+
+  return productos;
+};
+
+filtrarPorCategoria = (productos, categoria) => {
+  let productosFiltrados = productos.filter((producto) => {
+    return producto.categoria === categoria;
+  });
+
+  return productosFiltrados;
+};
+
+
+sumarPrecios = (productos) => {
+  let total = 0;
+
+  for (let i = 0; i < productos.length; i++) {
+    total += productos[i].precio;
+  }
+
+  return total;
+};
+
+let listaProductos = addProductosPrompt();
+console.log("Lista de productos:", listaProductos);
+
+let categoriaFiltrada = "ojos";
+let productosFiltrados = filtrarPorCategoria(listaProductos, categoriaFiltrada);
+console.log(`Productos de la categoría "${categoriaFiltrada}":`, productosFiltrados);
+
+let TotalPrecios = sumarPrecios(listaProductos);
+console.log("Total de precios", TotalPrecios);
